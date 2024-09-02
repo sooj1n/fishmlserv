47934594999177f81382b0d38698a67f0c26a79f
sooj1n <sujin19371@gmail.com>
2024-09-02 14:25:17 +0900

    cache

---
+++ src/fishmlserv/model/manager.py
@@ -0,0 +1,19 @@
+import os
+
+def get_model_path():
+    # 이 함수 파일의 절대 경로를 받아온다.
+    my_pyth = __file__
+    #/home/sujin/code/fishmlserv/src/fishmlserv/model/manager.py
+    dir_name = os.path.dirname(my_path)
+    #/home/sujin/code/fishmlserv/src/fishmlserv/model
+
+    # 절대 경로를 이용해 model.pkl의 경로를 조합
+    #model_path = dir_name + "/" + "model.pkl"
+    model_path = os.path.join(dir_name, "model.pkl")
+
+    # 조합된 경로를 리턴 - 끝
+    return model_path
+
+    # 사용 fastapi mian.py에서 아래와 같이 사용
+    # from fishmlserv.model.manager import get_model_path
+
