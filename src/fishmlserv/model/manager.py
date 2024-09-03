5da8af2f4d3010b7151299be4070b5b5a2f7f8f2
sooj1n <sujin19371@gmail.com>
2024-09-03 14:39:13 +0900

    argv

--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import pickle
 
 def get_model_path():
     # 이 함수 파일의 절대 경로를 받아온다.
@@ -17,7 +19,10 @@ def get_model_path():
     # 사용 fastapi mian.py에서 아래와 같이 사용
     # from fishmlserv.model.manager import get_model_path
 
-def prediction(l:float, w:float):
+def prediction():
+    l = float(sys.argv[1])
+    w = float(sys.argv[2])
+
     model_path = get_model_path()
 
     ### 모델 불러오기
